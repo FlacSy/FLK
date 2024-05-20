333aefbdd69d77418662606ff7e39d6f6c826be3
FlacSy <flacsy.x@gmail.com>
2024-05-20 22:38:35 +0200

    1.0.4

--- flk/flk.py
+++ flk/flk.py
@@ -1,4 +1,5 @@
 import re
+import argparse
 from typing import Any, Dict, Union
 
 class Variable:
@@ -285,3 +286,31 @@ class Parser:
                     multiline_buffer += line + " "
                     
         return {name: variable.value for name, variable in self.data.items()}
+
+def parse_args():
+    """
+    Разбирает аргументы командной строки.
+
+    Возвращает объект с аргументами, где 'filename' - имя файла, который нужно спарсить.
+    """
+    parser = argparse.ArgumentParser(description="Парсер файлов")
+    parser.add_argument('filename', type=str, help='Имя файла для парсинга')
+    return parser.parse_args()
+
+def main():
+    """
+    Основная функция для запуска парсера из командной строки.
+
+    Использует аргументы командной строки для определения файла для парсинга и выводит результаты.
+    Обрабатывает исключения, возникающие в процессе парсинга.
+    """
+    args = parse_args()
+    parser = Parser()
+    try:
+        data = parser.parse_file(args.filename)
+        print(data)
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flk',
-    version='1.0.3',
+    version='1.0.4',
     description='FLK - это библиотека для парсинга и работы с файлами в формате FL (File Language).',
     author='FlacSy',
     author_email='flacsy.tw@gmail.com',
